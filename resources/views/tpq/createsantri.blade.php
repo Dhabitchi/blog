2b665900350609c08e602d28852b4d75f21c5f66
Dhabitchi <60819843+Dhabitchi@users.noreply.github.com>
2020-02-22 14:49:39 +0700

    fix santri

--- resources/views/tpq/createsantri.blade.php
+++ resources/views/tpq/createsantri.blade.php
@@ -8,11 +8,33 @@
             Nama:<br>
             <input type="text" name="nama" value="">
             <br>
-            Kelas:<br>
-            <input type="text" name="kelas" value="">
+            Kelas :<br>
+            <select name="kelas">
+                @php($kelas = ['PAUD', 'TK', '1 SD', '2 SD', '3 SD', '4 SD', '5 SD', '6 SD', '1 SMP', '2 SMP', '3 SMP'])
+                @foreach($kelas as $k)
+                    <option value="{{ $k }}">{{ $k }}</option>
+                @endforeach
+            </select>
             <br>
-            Iqra/Al'quran:<br>
-            <input type="text" name="jilid" value="">
+            Progres  :<br>
+            @php($jilid = (function () {
+                $jilid = collect();
+
+                for($i = 1; $i <= 6; $i++) {
+                    $jilid->push("Iqra': Jilid {$i}");
+                }
+
+                for($i = 1; $i <= 30; $i++) {
+                    $jilid->push("Al-Qur'an: Juz {$i}");
+                }
+
+                return $jilid;
+            })())
+            <select name="jilid">
+                @foreach($jilid as $j)
+                    <option value="{{ $j }}">{{ $j }}</option>
+                @endforeach
+            </select>
             <br><br>
             <input type="submit" value="Submit" class="btn btn-dark btn-sm">
             <a href="{{route('santri')}}" class="btn btn-dark btn-sm">Back</a>
--- resources/views/tpq/editsantri.blade.php
+++ resources/views/tpq/editsantri.blade.php
@@ -10,10 +10,32 @@
             <input type="text" name="nama" value="{{$santri->nama}}">
             <br>
             Kelas :<br>
-            <input type="text" name="kelas" value="{{$santri->kelas}}">
+            <select name="kelas">
+                @php($kelas = ['PAUD', 'TK', '1 SD', '2 SD', '3 SD', '4 SD', '5 SD', '6 SD', '1 SMP', '2 SMP', '3 SMP'])
+                @foreach($kelas as $k)
+                    <option value="{{ $k }}" @if($santri->kelas == $k) selected @endif>{{ $k }}</option>
+                @endforeach
+            </select>
             <br>
-            Jilid :<br>
-            <input type="text" name="jilid" value="{{$santri->jilid}}">
+            Progres  :<br>
+            @php($jilid = (function () {
+                $jilid = collect();
+
+                for($i = 1; $i <= 6; $i++) {
+                    $jilid->push("Iqra': Jilid {$i}");
+                }
+
+                for($i = 1; $i <= 30; $i++) {
+                    $jilid->push("Al-Qur'an: Juz {$i}");
+                }
+
+                return $jilid;
+            })())
+            <select name="jilid">
+                @foreach($jilid as $j)
+                    <option value="{{ $j }}" @if($santri->jilid == $j) selected @endif>{{ $j }}</option>
+                @endforeach
+            </select>
             <br><br>
             <input type="submit" value="Submit" class="btn btn-dark btn-sm">
             <a href="{{route('santri')}}" class="btn btn-dark btn-sm">Back</a>
